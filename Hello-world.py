382ed4e72a1d1eb96e25140448461680a4845cf3
Aya Osama Ahmed <37303559+AyaOsamaAhmed@users.noreply.github.com>
2023-03-07 14:48:17 +0200

    Add files via upload
    first commit

---
+++ Hello-world.py
@@ -0,0 +1,52 @@
+
+"""
+name = input("What is your name? ")
+print(name)
+
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+"""
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Paulo"])
+
+
+"""
+print("Hello World--")
+print("Python has three numeric types: int, float, and complex")
+myValue = 1
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValueFloat = 3.14
+print(type(myValueFloat))
+print(str(myValueFloat) + " is of the data type " + str(type(myValueFloat)))
+myValueComplex = 5j
+print(type(myValueComplex))
+print(str(myValueComplex) + " is of the data type " + str(type(myValueComplex)))
+myValueBoolean = True
+print(type(myValueBoolean))
+print(str(myValueBoolean) + " is of the data type " + str(type(myValueBoolean)))
+myValueBoolean = False
+print(type(myValueBoolean))
+print(str(myValueBoolean) + " is of the data type " + str(type(myValueBoolean)))
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(str(myString) + " is of the data type " + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+"""
---
+++ test.py
@@ -0,0 +1,18 @@
+# input Full name , age
+# output 
+
+name = input("what's your name? ")
+
+age = int(input("what's your age? ")) 
+
+firstName = name.
+print(f' first name is {firstName}')
+print(f' middle name is {middleName}')
+print(f' last name is {lastName}')
+
+if age >= 18 :
+    print("you are a college student")
+else :
+    print("you are a student student")
+ 
+
